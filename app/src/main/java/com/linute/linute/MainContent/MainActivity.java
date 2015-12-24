6db7d11d6b423ebcef06ca26504e0d108a5b681a
Mohammed Amin <arta0613@gmail.com>
2015-12-23 19:56:17 -0500

    Add Discover Feed

--- app/src/main/java/com/linute/linute/MainContent/MainActivity.java
+++ app/src/main/java/com/linute/linute/MainContent/MainActivity.java
@@ -1,11 +1,14 @@
 package com.linute.linute.MainContent;
 
 import android.os.PersistableBundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 
 import com.linute.linute.API.API_Methods;
 import com.linute.linute.API.LSDKUser;
@@ -34,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
         //get toolbar
         mToolbar = (Toolbar) findViewById(R.id.mainactivity_toolbar);
         setSupportActionBar(mToolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         mViewPager = (ViewPager) findViewById(R.id.mainactivity_viewpager);
