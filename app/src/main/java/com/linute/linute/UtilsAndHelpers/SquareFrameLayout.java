d69bbb8bdd77da5532eb9a389e782a53c85f1e30
Qi Feng Huang <qifenghuang2@gmail.com>
2016-02-27 12:18:37 -0500

    updates

---
+++ app/src/main/java/com/linute/linute/UtilsAndHelpers/SquareFrameLayout.java
@@ -0,0 +1,23 @@
+package com.linute.linute.UtilsAndHelpers;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by QiFeng on 2/27/16.
+ *
+ * NOTE: this layout always uses width
+ */
+public class SquareFrameLayout extends FrameLayout {
+
+
+    public SquareFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
