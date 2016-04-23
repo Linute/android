d1977736f13b8657b91aa90c2ca54f0137400ebe
Qi Feng Huang <qifenghuang2@gmail.com>
2016-04-23 18:38:23 -0400

    test;

---
+++ app/src/main/java/com/linute/linute/UtilsAndHelpers/CustomImageFlipper.java
@@ -0,0 +1,25 @@
+package com.linute.linute.UtilsAndHelpers;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageSwitcher;
+import android.widget.ViewSwitcher;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by QiFeng on 4/23/16.
+ */
+public class CustomImageFlipper extends ViewSwitcher {
+    public CustomImageFlipper(Context context) {
+        super(context);
+    }
+
+    public CustomImageFlipper(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+
+}
