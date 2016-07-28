17d56511e5b96e7b54ff45dfe8dbcc90f5c85752
Mikhail Foenko <mfoenko@gmail.com>
2016-07-28 12:52:06 -0400

    removed chat icons from chat room

--- app/src/main/java/com/linute/linute/MainContent/Chat/ChatFragment.java
+++ app/src/main/java/com/linute/linute/MainContent/Chat/ChatFragment.java
@@ -27,12 +27,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
-import com.bumptech.glide.signature.StringSignature;
 import com.linute.linute.API.API_Methods;
 import com.linute.linute.API.LSDKChat;
 import com.linute.linute.MainContent.MainActivity;
@@ -753,9 +750,10 @@ public class ChatFragment extends BaseFragment implements LoadMoreViewHolder.OnL
         View rootV = getView();
         if (rootV == null) return;
         Toolbar toolbar = (Toolbar) rootV.findViewById(R.id.chat_fragment_toolbar);
+
+
+       /*
         ImageView otherPersonIconIV = (ImageView) toolbar.findViewById(R.id.toolbar_chat_user_icon);
-
-
         if (isDM() && mOtherPersonProfileImage == null) {
             otherPersonIconIV.setVisibility(View.GONE);
             return;
@@ -770,7 +768,7 @@ public class ChatFragment extends BaseFragment implements LoadMoreViewHolder.OnL
                 .placeholder(R.drawable.image_loading_background)
                 .diskCacheStrategy(DiskCacheStrategy.RESULT) //only cache the scaled image
                 .listener(mGlideListener)
-                .into(otherPersonIconIV);
+                .into(otherPersonIconIV);*/
 
         TextView chatNameView = (TextView) toolbar.findViewById(R.id.toolbar_chat_user_name);
         String chatName = getChatName();
