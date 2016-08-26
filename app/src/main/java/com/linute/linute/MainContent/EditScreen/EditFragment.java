3e399569cff24be2e5aaa82866ae4d4a457909cf
Mikhail Foenko <mfoenko@gmail.com>
2016-08-26 15:06:45 -0400

    set EditFragment to be immersive

--- app/src/main/java/com/linute/linute/MainContent/EditScreen/EditFragment.java
+++ app/src/main/java/com/linute/linute/MainContent/EditScreen/EditFragment.java
@@ -181,18 +181,17 @@ public class EditFragment extends BaseFragment {
 
     }
 
-    /*@Override
+    @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        View decorView = getActivity().getWindow().getDecorView();
-// Hide both the navigation bar and the status bar.
-// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-// a general rule, you should design your app to hide the status bar whenever you
-// hide the navigation bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-    }*/
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            View decorView = getActivity().getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
 
 
 
