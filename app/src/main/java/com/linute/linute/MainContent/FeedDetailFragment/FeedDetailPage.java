ce281c648778bbbc026d2a337dc470f4349de2ba
Mikhail Foenko <mfoenko@gmail.com>
2016-09-20 18:23:33 -0400

    fixed comment number not updating when returning to feed after commenting on a post

--- app/src/main/java/com/linute/linute/MainContent/FeedDetailFragment/FeedDetailPage.java
+++ app/src/main/java/com/linute/linute/MainContent/FeedDetailFragment/FeedDetailPage.java
@@ -1539,6 +1539,8 @@ public class FeedDetailPage extends BaseFragment implements QueryTokenReceiver,
             mProgressbar.setVisibility(View.VISIBLE);
             mCommentEditText.setText("");
             activity.emitSocket(API_Methods.VERSION + ":comments:new comment", comment);
+
+            mFeedDetail.getPost().setNumOfComments(mFeedDetail.getPost().getNumOfComments()+1);
         } catch (JSONException e) {
             e.printStackTrace();
             Utils.showServerErrorToast(getActivity());
