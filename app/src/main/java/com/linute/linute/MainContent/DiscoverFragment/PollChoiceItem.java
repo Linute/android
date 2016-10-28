c41ea08f741f8a75e2f1ef728c1a004f40b15486
Mikhail Foenko <mfoenko@gmail.com>
2016-10-28 10:41:22 -0400

    added check to make sure poll option colors are 6 digits (was causing crash for 3 digit colors)

--- app/src/main/java/com/linute/linute/MainContent/DiscoverFragment/PollChoiceItem.java
+++ app/src/main/java/com/linute/linute/MainContent/DiscoverFragment/PollChoiceItem.java
@@ -3,7 +3,6 @@ package com.linute.linute.MainContent.DiscoverFragment;
 import android.graphics.Color;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.support.annotation.ColorInt;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,12 +17,15 @@ public class PollChoiceItem implements Parcelable {
     public final int mColor;
     public final String id;
 
-    public PollChoiceItem(JSONObject object)throws JSONException{
+    public PollChoiceItem(JSONObject object) throws JSONException {
         mOptionText = object.getString("text");
         mVotes = object.getInt("votes");
         id = object.getString("id");
-        mColor = Color.parseColor("#"+object.getString("color"));
-
+        if(object.getString("color").length() == 6) {
+            mColor = Color.parseColor("#" + object.getString("color"));
+        }else {
+            mColor = 0;
+        }
     }
 
     protected PollChoiceItem(Parcel in) {
