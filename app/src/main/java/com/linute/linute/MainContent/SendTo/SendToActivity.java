60216f30f1926628efe9f6d861ef7baef36b1d6f
Mikhail Foenko <mfoenko@gmail.com>
2016-11-12 11:51:42 -0500

    fixed share over messenger

--- app/src/main/java/com/linute/linute/MainContent/SendTo/SendToActivity.java
+++ app/src/main/java/com/linute/linute/MainContent/SendTo/SendToActivity.java
@@ -3,16 +3,21 @@ package com.linute.linute.MainContent.SendTo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.linute.linute.Socket.TaptSocket;
+import com.linute.linute.UtilsAndHelpers.BaseTaptActivity;
 
 /**
  * Created by mikhail on 11/11/16.
  */
 
-public class SendToActivity extends AppCompatActivity{
+public class SendToActivity extends BaseTaptActivity{
 
 
     public static final String EXTRA_POST_ID = "post_id";
+    public static final String FRAG_BACKSTACK_NAME = "sendTo";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,7 +32,53 @@ public class SendToActivity extends AppCompatActivity{
             postId = intent.getStringExtra(EXTRA_POST_ID);
         }
 
-        getSupportFragmentManager().beginTransaction().add(SendToFragment.newInstance(postId),SendToFragment.TAG);
+        if(!TaptSocket.getInstance().socketConnected()){
+            TaptSocket.getInstance().connectSocket();
+        }
+
+        final FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().add(android.R.id.content, SendToFragment.newInstance(postId)).addToBackStack(FRAG_BACKSTACK_NAME).commit();
+        fm.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                if(fm.getBackStackEntryCount() == 0 || !FRAG_BACKSTACK_NAME.equals(fm.getBackStackEntryAt(0).getName())) {
+                    finish();
+                    fm.removeOnBackStackChangedListener(this);
+                }
+            }
+        });
     }
 
+    @Override
+    public void addFragmentToContainer(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().add(android.R.id.content, fragment).addToBackStack(null).commit();
+
+    }
+
+    @Override
+    public void addFragmentToContainer(Fragment fragment, String tag) {
+        getSupportFragmentManager().beginTransaction().add(android.R.id.content, fragment, tag).addToBackStack(null).commit();
+
+    }
+
+    @Override
+    public void replaceContainerWithFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, fragment).commit();
+    }
+
+    @Override
+    public void addFragmentOnTop(Fragment fragment, String tag) {
+        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, fragment).addToBackStack(null).commit();
+    }
+
+    @Override
+    public void setSocketErrorResponse(SocketErrorResponse error) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        TaptSocket.getInstance().disconnectSocket();
+    }
 }
