3de052df4b84ccca0e234e61127a5ec279069ba9
Qi Feng Huang <qifenghuang2@gmail.com>
2016-11-26 15:29:45 -0500

    handling empty posts

--- app/src/main/java/com/linute/linute/MainContent/Global/TrendingItemAdapter.java
+++ app/src/main/java/com/linute/linute/MainContent/Global/TrendingItemAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +13,12 @@ import android.widget.TextView;
 import com.bumptech.glide.RequestManager;
 import com.linute.linute.API.API_Methods;
 import com.linute.linute.MainContent.DiscoverFragment.BaseFeedItem;
+import com.linute.linute.MainContent.DiscoverFragment.BasePostFeedHolder;
 import com.linute.linute.MainContent.DiscoverFragment.ImageFeedHolder;
 import com.linute.linute.MainContent.DiscoverFragment.Poll;
 import com.linute.linute.MainContent.DiscoverFragment.PollViewHolder;
 import com.linute.linute.MainContent.DiscoverFragment.Post;
+import com.linute.linute.MainContent.DiscoverFragment.StatusFeedHolder;
 import com.linute.linute.MainContent.DiscoverFragment.VideoFeedHolder;
 import com.linute.linute.R;
 import com.linute.linute.Socket.TaptSocket;
@@ -60,34 +63,37 @@ public class TrendingItemAdapter extends BaseFeedAdapter {
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        if (viewType == VIDEO_POST) {
-            return new VideoTrendViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.trending_item_video, parent, false),
+        switch (viewType){
+            case VIDEO_POST:
+                return new VideoTrendViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.trending_item_video, parent, false),
                     mContext, mRequestManager, mPostAction);
-        } else if (viewType == IMAGE_POST) {
-            return new BaseTrendViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.trending_item, parent, false),
-                    mContext, mRequestManager, mPostAction);
-        } else if (viewType == LoadMoreViewHolder.FOOTER) {
-            return new LoadMoreViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.feed_load_more, parent, false),
-                    "",
-                    "That's all folks!"
-            );
-        }else if (viewType == POLL){
-            return new PollViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.feeddetail_poll, parent, false)
-                    //, mPostAction
-                    , null
-            );
+            case IMAGE_POST:
+                return new BaseTrendViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.trending_item, parent, false),
+                        mContext, mRequestManager, mPostAction);
+            case STATUS_POST:
+                return new StatusTrendViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.trending_item_status, parent, false),
+                        mContext, mRequestManager, mPostAction);
+            case LoadMoreViewHolder.FOOTER:
+                return new LoadMoreViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.feed_load_more, parent, false),
+                        "",
+                        "That's all folks!"
+                );
+            case POLL:
+                return new PollViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.feeddetail_poll, parent, false)
+                        //, mPostAction
+                        , null
+                );
+            default:
+                return null;
         }
 
-        return null;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
-        if (holder instanceof BaseTrendViewHolder) {
-            ((BaseTrendViewHolder) holder).bindModel((Post) mPosts.get(position));
-        } else if (holder instanceof VideoTrendViewHolder) {
-            ((VideoFeedHolder) holder).bindModel((Post) mPosts.get(position));
+        if (holder instanceof BasePostFeedHolder){
+            ((BasePostFeedHolder) holder).bindModel((Post) mPosts.get(position));
         } else if (holder instanceof LoadMoreViewHolder) {
             ((LoadMoreViewHolder) holder).bindView(mLoadState);
         } else if (holder instanceof PollViewHolder){
@@ -112,7 +118,7 @@ public class TrendingItemAdapter extends BaseFeedAdapter {
                 return VIDEO_POST;
             } else if (p.isImagePost()) {
                 return IMAGE_POST;
-            }
+            }else return STATUS_POST;
         } else if (mPosts.get(position) instanceof Poll) {
             return POLL;
         }
@@ -160,6 +166,22 @@ public class TrendingItemAdapter extends BaseFeedAdapter {
         }
     }
 
+    private class StatusTrendViewHolder extends StatusFeedHolder{
+        TextView vCollegeName;
+
+
+        StatusTrendViewHolder(View itemView, Context context, RequestManager manager, PostAction action) {
+            super(itemView, context, manager, action);
+            vCollegeName = (TextView) itemView.findViewById(R.id.college_name);
+        }
+
+        @Override
+        public void bindModel(Post post) {
+            super.bindModel(post);
+            vCollegeName.setText(post.getCollegeName());
+            sendImpressionsAsync(post.getId());
+        }
+    }
 
     private void sendImpressionsAsync(final String id) {
 
